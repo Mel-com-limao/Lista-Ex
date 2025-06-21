322a3e6955547c00ec5f1a4013ae2a9adc2b9aa2
Mel-com-limao <g.melissa@aluno.ifsp.edu.br>
2025-06-21 16:54:42 -0300

    Ex 1.js

---
+++ Ex 1.js
@@ -0,0 +1,9 @@
+function ParImpar(){
+    opcao = parseInt(document.formulario.opcao.value)
+    if (opcao%2==0){
+        alert("O número digitado é par")
+    }else{
+        alert("O número digitado é ímpar")
+    }
+}
+ParImpar
